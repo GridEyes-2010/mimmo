a2cbe69dd2cdaeffb91b4786e434785ff22627ba
edoardolombardi <edoardo.lombardi@optimad.it>
2020-07-10 12:03:42 +0200

    core: add clean all parallel sync method to MimmoObject

--- src/core/MimmoObject.cpp
+++ src/core/MimmoObject.cpp
@@ -2092,7 +2092,6 @@ void MimmoObject::resetPointGhostExchangeInfo()
 	m_pointGhostExchangeInfoSync = false;
 }
 
-
 /*!
     General checker for SkdTree status throughout all procs. If at least one partition
     has not-syncronized SkdTree set all the other procs to not syncronized and free the structure eventually.
@@ -2214,6 +2213,24 @@ bool MimmoObject::cleanParallelPointGhostExchangeInfoSync(){
 	}
 	//return true clean if the boolean was false, and viceversa.
 	return (!m_pointGhostExchangeInfoSync);
+}
+
+/*!
+    General checker for all InfoSync status throughout all procs. If at least one partition
+    has one not-syncronized InfoSync set all the other procs to not syncronized for this InfoSync
+    and free the structure eventually.
+    \return true if at least the structure was reset, false if nothing was done.
+ */
+bool MimmoObject::cleanAllParallelSync(){
+    bool reset = false;
+    reset |= cleanParallelSkdTreeSync();
+    reset |= cleanParallelKdTreeSync();
+    reset |= cleanParallelAdjacenciesSync();
+    reset |= cleanParallelInterfacesSync();
+    reset |= cleanParallelPointConnectivitySync();
+    reset |= cleanParallelInfoSync();
+    reset |= cleanParallelPointGhostExchangeInfoSync();
+    return reset;
 }
 
 /*!
